ca26deec3d4d43a187dcde060cc1677117c16d91
Luana-s <luana.silva1@arapiraca.ufal.br>
2022-12-12 15:39:56 -0300

    sobre mim

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState} from 'react'
 import { Header } from '../components/Header'
 import homeImg from '../assets/portfolio.svg'
+import Axios from 'axios'
 export function Portfolio(){
-    return(
+    const[github, setGithub]=useState([])
+    useEffect(()=> {
+        async function pegaDados(){
+            const resposta= await Axios.get('https://api.github.com/users/Luana-s/repos',target="_blank")
+            console.log(resposta.data.results)
+        }
+        pegaDados()
+    }, [])
+        return(
         <>
+
         <Header text="Meu Portfolio✔️" image={homeImg} />
-        
+
+        {github.map(repositorio => {
+            return(
+                <p key={repositorio.name}>{repositorio.name}</p>
+            )
+
+        })}
         </>
-    )
+        )
 }
--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,10 +1,26 @@
 import { Header } from '../components/Header'
 import homeImg from '../assets/sobre.svg'
+import styles from '../styles/pages/sobre.module.css'
+
+
+
+
 export function Sobre(){
     return(
         <>
          <Header text="Sobre" image={homeImg} />
-        
+
+        <section className={styles.card}>
+            <div>
+                <h1 className={styles.name}>Luana Silva,</h1>
+                <br/>
+                <p className={styles.texto}>Olá, sou de Alagoas, tenho 21 anos. Sempre fui apaixonada por tecnologia, que mim desperta um desejo de mergulhar em coisas novas, desafios. </p>
+                <br/>
+                <p className={styles.texto}>Atualmente faço Ciências da Computação e busco também outros conhecimentos relacionados a tecnologia!</p>
+                <img  className={styles.imagem} src="src/assets/luana.jpg" alt="minha foto"/>
+            </div> 
+        </section>
+         
         </>
     )
 }
